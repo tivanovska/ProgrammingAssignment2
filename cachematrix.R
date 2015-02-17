169a9b0855752dd82cb63539fb32e585ec218b63
tivanovska <tiva@uni-greifswald.de>
2015-02-17 14:04:34 +0100

    solution of Ivanovska T.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## special matrix type that can stores the inverted matrix
+##
 
 makeCacheMatrix <- function(x = matrix()) {
+  invMatr <-NULL
+  set <- function(y) {
+      x <<- y
+      invMatr <<- NULL
+    }
+  get <- function() x
+  setInvMatr <- function(matI) invMatr <<- matI
+  getInvMatr <- function() invMatr
+  list(set = set, get = get,
+             setInvMatr = setInvMatr,
+             getInvMatr = getInvMatr)
+   
 
 }
 
 
-## Write a short comment describing this function
-
+## the function computes the inverted matrix, if nothing is in cache 
+## otherwise it returns the cached matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   ## Return a matrix that is the inverse of 'x' 
+    m<-x$getInvMatr()
+    if(!is.null(m)) {
+        message("getting cached inverted matrix")
+        return(m)
+    }
+    data <-x$get()
+    m<-solve(data)
+    x$setInvMatr(m)
+    m
 }
